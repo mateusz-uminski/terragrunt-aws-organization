1b19a76fabaf3cd855295752fa2252ae86311ba3
Mateusz Umiński <17390661+mateusz-uminski@users.noreply.github.com>
2023-06-11 22:57:32 +0200

    feat: configure IAM Identity Center for a given project

---
+++ microcloud-management/.settings/iam-identity-center-microdata/main.tf
@@ -0,0 +1,16 @@
+variable "aws_accounts" {
+  type = list(object({
+    account_name = string
+    account_tier = string
+    account_id   = string
+  }))
+  description = "Defined in the terragrunt.hcl file."
+}
+
+module "iam_identity_center_microdata" {
+  source = "git::https://github.com/mateusz-uminski/terraform-aws-modules//iam-identity-center-project?ref=iam-identity-center-project/v0.1.0"
+
+  org_abbreviated_name = var.org_abbreviated_name
+  aws_accounts         = var.aws_accounts
+  project_name         = "microdata"
+}
