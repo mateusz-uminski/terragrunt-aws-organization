5490dc8387cc4f3249519b13fa8b84283ac665b3
Mateusz Umiński <17390661+mateusz-uminski@users.noreply.github.com>
2023-06-18 20:58:24 +0200

    feat: create a bucket for vpc flow logs

---
+++ microcloud-shared/.settings/vpc-flow-logs-bucket/main.tf
@@ -0,0 +1,19 @@
+variable "allowed_account_ids" {
+  type        = list(string)
+  description = "Defined in the terragrunt.hcl file."
+}
+
+module "vpc_flow_logs_bucket" {
+  source = "git::https://github.com/mateusz-uminski/terraform-aws-modules//vpc-flow-logs-bucket?ref=vpc-flow-logs-bucket/v0.1.0"
+
+  # required variables
+  org_abbreviated_name = var.org_abbreviated_name
+  account_id           = var.account_id
+
+  # optional variables
+  allowed_account_ids = var.allowed_account_ids
+}
+
+output "arn" {
+  value = module.vpc_flow_logs_bucket.arn
+}
