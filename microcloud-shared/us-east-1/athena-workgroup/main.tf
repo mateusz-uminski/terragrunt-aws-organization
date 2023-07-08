3bde76d5525630e2a2bf1e00cadc3b00dfdd34e6
Mateusz Umiński <17390661+mateusz-uminski@users.noreply.github.com>
2023-07-08 23:35:48 +0200

    feat: create main athena workgroup in the shared account

---
+++ microcloud-shared/us-east-1/athena-workgroup/main.tf
@@ -0,0 +1,8 @@
+module "athena_workgroup" {
+  source = "git::https://github.com/mateusz-uminski/terraform-aws-modules//athena-workgroup?ref=athena-workgroup/v0.1.0"
+
+  # required variables
+  org_abbreviated_name = var.org_abbreviated_name
+  workgroup_name       = "main"
+  bucket_owner         = var.account_id
+}
