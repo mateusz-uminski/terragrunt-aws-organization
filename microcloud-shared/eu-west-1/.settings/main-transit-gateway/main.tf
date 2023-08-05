be36cfb805eba4161d121acbbd6c1aad29bb81cd
Mateusz Umiński <17390661+mateusz-uminski@users.noreply.github.com>
2023-08-05 19:01:49 +0200

    feat: create transit gateways in the shared account

---
+++ microcloud-shared/eu-west-1/.settings/main-transit-gateway/main.tf
@@ -0,0 +1,17 @@
+module "transit_gateway" {
+  source = "git::https://github.com/mateusz-uminski/terraform-aws-modules//transit-gateway?ref=transit-gateway/v0.1.0"
+
+  # required variables
+  org_abbreviated_name = var.org_abbreviated_name
+  name                 = "main"
+  asn                  = "65020"
+  route_table_names    = ["shared", "nonprod", "prod"]
+}
+
+output "id" {
+  value = module.transit_gateway.id
+}
+
+output "arn" {
+  value = module.transit_gateway.arn
+}
