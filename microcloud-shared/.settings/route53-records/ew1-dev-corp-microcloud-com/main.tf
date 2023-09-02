3d73aba78010e6c019a591a640c24675c0067e9c
Mateusz Umiński <17390661+mateusz-uminski@users.noreply.github.com>
2023-09-02 22:00:07 +0200

    feat: create a route53 example record in each private zone

---
+++ microcloud-shared/.settings/route53-records/ew1-dev-corp-microcloud-com/main.tf
@@ -0,0 +1,17 @@
+variable "hosted_zone_id" {
+  type        = string
+  description = "Defined in the terragrunt.hcl file."
+}
+
+locals {
+  hosted_zone_name = "ew1.dev.corp.microcloud.com"
+}
+
+resource "aws_route53_record" "example" {
+  zone_id = var.hosted_zone_id
+
+  name    = "example.${local.hosted_zone_name}"
+  type    = "CNAME"
+  ttl     = 300
+  records = ["example"]
+}
