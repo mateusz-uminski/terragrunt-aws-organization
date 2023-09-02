dca2badcce2d84426c2da4d509691b6345603462
Mateusz Umiński <17390661+mateusz-uminski@users.noreply.github.com>
2023-09-02 16:47:39 +0200

    feat: create simple ec2 instances in nonprod account for testing network configuration

---
+++ microcloud-nonprod/eu-west-1/private-ec2/dev/main.tf
@@ -0,0 +1,23 @@
+module "ec2" {
+  source = "git::https://github.com/mateusz-uminski/terraform-aws-modules//ec2?ref=ec2/v0.1.0"
+
+  # required variables
+  org_abbreviated_name = var.org_abbreviated_name
+  project              = "infra"
+  environment          = "dev"
+
+  instance_name    = "private"
+  instance_type    = "t2.micro"
+  ami_name_pattern = "CentOS-Stream-ec2-9-*"
+  key_pair         = "mcd-main-key-pair"
+
+  vpc_name    = "mcd-main-vpc-nonprod"
+  subnet_name = "mcd-main-private-sn1-nonprod"
+
+  # optional variables
+  assign_public_ip = false
+
+  additional_security_groups = []
+  allowed_ingress_cidrs      = []
+  allowed_ingress_sgs        = []
+}
