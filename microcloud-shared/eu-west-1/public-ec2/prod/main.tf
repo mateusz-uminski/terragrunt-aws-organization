388a853814ce672671d0162bc9608b34d8418fb9
Mateusz Umiński <17390661+mateusz-uminski@users.noreply.github.com>
2023-09-02 16:15:52 +0200

    feat: create simple ec2 instances in shared account for testing network configuration

---
+++ microcloud-shared/eu-west-1/public-ec2/prod/main.tf
@@ -0,0 +1,23 @@
+module "ec2" {
+  source = "git::https://github.com/mateusz-uminski/terraform-aws-modules//ec2?ref=ec2/v0.1.0"
+
+  # required variables
+  org_abbreviated_name = var.org_abbreviated_name
+  project              = "infra"
+  environment          = "prod"
+
+  instance_name    = "public"
+  instance_type    = "t2.micro"
+  ami_name_pattern = "CentOS-Stream-ec2-9-*"
+  key_pair         = "mcd-main-key-pair"
+
+  vpc_name    = "mcd-main-vpc-shared"
+  subnet_name = "mcd-main-public-sn1-shared"
+
+  # optional variables
+  assign_public_ip = true
+
+  additional_security_groups = []
+  allowed_ingress_cidrs      = []
+  allowed_ingress_sgs        = []
+}
