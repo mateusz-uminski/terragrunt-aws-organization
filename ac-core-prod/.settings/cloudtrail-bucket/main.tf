aff8d27277329324208578ad3dbee4b4411b60c5
Mateusz Umiński <17390661+mateusz-uminski@users.noreply.github.com>
2024-10-19 16:34:29 +0200

    fix: set correct soruce arns in a cloudtrail bucket (#21)

--- ac-core-prod/.settings/cloudtrail-bucket/main.tf
+++ ac-core-prod/.settings/cloudtrail-bucket/main.tf
@@ -4,7 +4,7 @@ variable "allowed_account_ids" {
 }
 
 module "cloudtrail_bucket" {
-  source = "git::https://github.com/mateusz-uminski/terraform-aws-modules//logs-bucket?ref=logs-bucket-v0.1.0"
+  source = "git::https://github.com/mateusz-uminski/terraform-aws-modules//logs-bucket?ref=logs-bucket-v0.2.0"
 
   # required variables
   org_code     = var.org_code
@@ -13,6 +13,7 @@ module "cloudtrail_bucket" {
   account_id   = var.account_id
 
   # optional variables
+  service_arn_prefix  = "arn:aws:cloudtrail"
   allowed_account_ids = concat([var.account_id], var.allowed_account_ids)
 }
 
